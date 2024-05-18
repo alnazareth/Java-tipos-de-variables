cdb982247ab08d199e2517e11905e6efb264a922
alnazareth <36893691+alnazareth@users.noreply.github.com>
2024-05-18 13:27:37 -0400

    initial update

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,22 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+ 
+    public static void main(String[] args) {
+       
+      //Tipos de variables
+        int entero=5;
+        String nombre="Alnazareth";
+        char letra='A';
+        double temperatura=38.5;
+        boolean bandera =false;
+        long enterolargo=123456789;
+        
+        
+
+    }
+    
+}
